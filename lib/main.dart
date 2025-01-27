cbb0b1a97819ceae356d9db0cface01ec4832f77
Dureksha <22ug1-0263@sltc.ac.lk>
2025-01-27 20:50:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'screens/input_screen.dart';
+import 'screens/result_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'NIC Decoder',
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => InputScreen()),
+        GetPage(name: '/result', page: () => ResultScreen()),
+      ],
+    );
+  }
+}
